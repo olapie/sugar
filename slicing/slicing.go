cba7aa730ae53684cb138e6166a7de22625d674e
yxz6 <yxzbox@gmail.com>
2022-12-05 21:34:58 -0500

    Update

--- slicing/slicing.go
+++ slicing/slicing.go
@@ -1,6 +1,11 @@
 package slicing
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+
+	"code.olapie.com/sugar/rtx"
+)
 
 func Clone[T any](a []T) []T {
 	res := make([]T, len(a))
@@ -75,6 +80,28 @@ func Reverse[E comparable](a []E) {
 	for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
 		a[i], a[j] = a[j], a[i]
 	}
+}
+
+func ReverseArray(a any) bool {
+	a = rtx.Indirect(a)
+	if a == nil {
+		return false
+	}
+	v := reflect.ValueOf(a)
+	if v.IsNil() || !v.IsValid() || v.Kind() != reflect.Array && v.Kind() != reflect.Slice {
+		return false
+	}
+
+	for i, j := 0, v.Len()-1; i < j; i, j = i+1, j-1 {
+		vi, vj := v.Index(i), v.Index(j)
+		tmp := vi.Interface()
+		if !vi.CanSet() {
+			return false
+		}
+		vi.Set(vj)
+		vj.Set(reflect.ValueOf(tmp))
+	}
+	return true
 }
 
 func Remove[E comparable](a []E, v E) []E {
--- slicing/slicing_test.go
+++ slicing/slicing_test.go
@@ -1,34 +1,34 @@
-package arrayx_test
+package slicing_test
 
 import (
 	"testing"
 
-	"code.olapie.com/sugar/arrayx"
+	"code.olapie.com/sugar/slicing"
 	"code.olapie.com/sugar/testx"
 )
 
 func TestReverse(t *testing.T) {
 	t.Run("IntArray", func(t *testing.T) {
 		a := []int{1, 2, 3, -9, 10, 1, 101}
-		arrayx.Reverse(a)
+		slicing.Reverse(a)
 		testx.Equal(t, []int{101, 1, 10, -9, 3, 2, 1}, a)
 
 		a = []int{1}
-		arrayx.Reverse(a)
+		slicing.Reverse(a)
 		testx.Equal(t, []int{1}, a)
 
 		a = []int{}
-		arrayx.Reverse(a)
+		slicing.Reverse(a)
 		testx.Equal(t, []int{}, a)
 
 		a = []int{1, 3}
-		arrayx.Reverse(a)
+		slicing.Reverse(a)
 		testx.Equal(t, []int{3, 1}, a)
 	})
 
 	t.Run("StringArray", func(t *testing.T) {
 		a := []string{"a", "b", "c", "d"}
-		arrayx.Reverse(a)
+		slicing.Reverse(a)
 		testx.Equal(t, []string{"d", "c", "b", "a"}, a)
 	})
 }
