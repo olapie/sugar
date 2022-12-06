ead5500279000714380cddba83d47d436c6def50
yxz6 <yxzbox@gmail.com>
2022-12-05 21:58:07 -0500

    Update

---
+++ httpx/conversion.go
@@ -0,0 +1,176 @@
+package httpkit
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"reflect"
+	"strings"
+
+	"code.olapie.com/sugar/types"
+)
+
+type MapOutput interface {
+	map[string]string | map[string]any | types.M
+}
+
+type MapInput interface {
+	http.Header | []*http.Cookie | url.Values
+}
+
+func ToMap[IN MapInput, OUT MapOutput](input IN) OUT {
+	var out OUT
+	var res any
+	_, isMapString := any(out).(map[string]string)
+	switch v := any(input).(type) {
+	case http.Header:
+		if isMapString {
+			res = headerToMapString(v)
+		} else {
+			res = headerToMap(v)
+		}
+	case []*http.Cookie:
+		if isMapString {
+			res = cookiesToMapString(v)
+		} else {
+			res = cookiesToMap(v)
+		}
+	case url.Values:
+		if isMapString {
+			res = valuesToMapString(v)
+		} else {
+			res = valuesToMap(v)
+		}
+	}
+
+	if res != nil {
+		reflect.ValueOf(&out).Elem().Set(reflect.ValueOf(res))
+	}
+	return out
+}
+
+func ToMapAny[T MapInput](v T) map[string]any {
+	return ToMap[T, map[string]any](v)
+}
+
+func ToMapString[T MapInput](v T) map[string]string {
+	return ToMap[T, map[string]string](v)
+}
+
+func ToM[T MapInput](v T) types.M {
+	return ToMap[T, types.M](v)
+}
+
+func headerToMap(h http.Header) map[string]any {
+	m := make(map[string]any, len(h))
+	for k, v := range h {
+		if len(v) == 0 {
+			continue
+		}
+
+		if len(v) == 1 {
+			m[k] = v[0]
+			continue
+		}
+		m[k] = v
+		k = strings.ToLower(k)
+		if strings.HasPrefix(k, "x-") {
+			k = k[2:]
+			k = strings.Replace(k, "-", "_", -1)
+			if _, ok := m[k]; !ok {
+				m[k] = v
+			}
+		}
+	}
+	return m
+}
+
+func headerToMapString(h http.Header) map[string]string {
+	m := make(map[string]string, len(h))
+	for k, v := range h {
+		if len(v) == 0 {
+			continue
+		}
+		m[k] = v[0]
+		k = strings.ToLower(k)
+		if strings.HasPrefix(k, "x-") {
+			k = k[2:]
+			k = strings.Replace(k, "-", "_", -1)
+			if _, ok := m[k]; !ok {
+				m[k] = v[0]
+			}
+		}
+	}
+	return m
+}
+
+func cookiesToMapString(cookies []*http.Cookie) map[string]string {
+	m := make(map[string]string, len(cookies))
+	for _, c := range cookies {
+		m[c.Name] = c.Value
+	}
+	return m
+}
+
+func cookiesToMap(cookies []*http.Cookie) map[string]any {
+	m := make(map[string]any, len(cookies))
+	for _, c := range cookies {
+		m[c.Name] = c.Value
+	}
+	return m
+}
+
+func valuesToMap(values url.Values) map[string]any {
+	m := map[string]any{}
+	for k, va := range values {
+		isArray := strings.HasSuffix(k, "[]")
+		if isArray {
+			k = k[0 : len(k)-2]
+			if k == "" {
+				continue
+			}
+
+			if len(va) == 1 {
+				va = strings.Split(va[0], ",")
+			}
+		}
+
+		if len(va) == 0 {
+			continue
+		}
+
+		k = strings.ToLower(k)
+		if isArray || len(va) > 1 {
+			// value is an array or expected to be an array
+			m[k] = va
+		} else {
+			m[k] = va[0]
+		}
+	}
+
+	if jsonStr, _ := m["json"].(string); jsonStr != "" {
+		var j map[string]any
+		err := json.Unmarshal([]byte(jsonStr), &j)
+		if err == nil {
+			for k, v := range m {
+				j[k] = v
+			}
+			m = j
+		}
+	}
+	return m
+}
+
+func valuesToMapString(values url.Values) map[string]string {
+	m := valuesToMap(values)
+	res := make(map[string]string, len(m))
+	for k, v := range m {
+		if s, ok := v.(string); ok {
+			res[k] = s
+		} else {
+			res[k] = fmt.Sprint(v)
+		}
+	}
+	return res
+}
---
+++ httpx/sign.go
@@ -0,0 +1,136 @@
+package httpkit
+
+import (
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+const (
+	KeyTimestamp = "X-Timestamp"
+	KeySignature = "X-Signature"
+)
+
+type Signer interface {
+	Sign(req *http.Request) error
+}
+
+type SignerFunc func(req *http.Request) error
+
+func (h SignerFunc) Sign(req *http.Request) error {
+	return h(req)
+}
+
+type PrivateKey interface {
+	ecdsa.PrivateKey | rsa.PrivateKey
+}
+
+func Sign[K PrivateKey](req *http.Request, priv *K) error {
+	ts := fmt.Sprint(time.Now().Unix())
+	req.Header.Set(KeyTimestamp, ts)
+	hash := getMessageHashForSigning(req)
+	var sign []byte
+	var err error
+	switch k := any(priv).(type) {
+	case *ecdsa.PrivateKey:
+		sign, err = ecdsa.SignASN1(rand.Reader, k, hash)
+	case *rsa.PrivateKey:
+		sign, err = rsa.SignPKCS1v15(rand.Reader, k, crypto.SHA256, hash)
+	default:
+		err = fmt.Errorf("invalid private key: %T", k)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set(KeySignature, base64.StdEncoding.EncodeToString(sign))
+	return nil
+}
+
+func GetSigner[K PrivateKey](priv *K) Signer {
+	return SignerFunc(func(req *http.Request) error {
+		return Sign(req, priv)
+	})
+}
+
+type Verifier interface {
+	Verify(req *http.Request) bool
+}
+
+type VerifierFunc func(req *http.Request) bool
+
+func (h VerifierFunc) Verify(req *http.Request) bool {
+	return h(req)
+}
+
+type PublicKey interface {
+	ecdsa.PublicKey | rsa.PublicKey
+}
+
+func Verify[K PublicKey](req *http.Request, pub *K) bool {
+	ts := req.Header.Get(KeyTimestamp)
+	t, err := strconv.ParseInt(ts, 0, 64)
+	if err != nil {
+		return false
+	}
+
+	if time.Now().Unix()-t > 5 {
+		return false
+	}
+
+	sign, err := base64.StdEncoding.DecodeString(req.Header.Get(KeySignature))
+	if err != nil {
+		return false
+	}
+	hash := getMessageHashForSigning(req)
+	switch k := any(pub).(type) {
+	case *ecdsa.PublicKey:
+		return ecdsa.VerifyASN1(k, hash, sign)
+	case *rsa.PublicKey:
+		return rsa.VerifyPKCS1v15(k, crypto.SHA256, hash, sign) == nil
+	default:
+		return false
+	}
+}
+
+func GetVerifier[K PublicKey](pub *K) Verifier {
+	return VerifierFunc(func(req *http.Request) bool {
+		return Verify(req, pub)
+	})
+}
+
+func getMessageHashForSigning(req *http.Request) []byte {
+	ts := req.Header.Get(KeyTimestamp)
+	auth := req.Header.Get(KeyAuthorization)
+	path := req.URL.Path
+	if path == "" {
+		path = req.URL.RawPath
+	}
+	message := req.Method + path + auth + ts
+	hash := sha256.Sum256([]byte(message))
+	return hash[:]
+}
+
+func example() {
+	// generate and marshal
+	privateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	pubData, _ := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+	priData, _ := x509.MarshalECPrivateKey(privateKey)
+
+	// parse
+	pri, _ := x509.ParseECPrivateKey(priData)
+	anyPub, _ := x509.ParsePKIXPublicKey(pubData)
+	pub := anyPub.(*ecdsa.PublicKey)
+	_ = pri
+	_ = pub
+}
