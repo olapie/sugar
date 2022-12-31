249660f33ca28b03733fd9b9aecd54fed9787bd8
yxz6 <yxzbox@gmail.com>
2022-12-30 23:02:51 -0500

    fast commit

--- v2/mob/time.go
+++ v2/mob/time.go
@@ -1,6 +1,9 @@
 package mob
 
-import "code.olapie.com/sugar/v2/xtime"
+import (
+	"code.olapie.com/sugar/v2/xtime"
+	"fmt"
+)
 
 func GetDateTimeString(t int64) string {
 	tm := xtime.TimeWithUnix(t)
@@ -20,4 +23,12 @@ func NowTime() *Time {
 
 func TimeWithUnix(seconds int64) *Time {
 	return xtime.TimeWithUnix(seconds)
+}
+
+func TimerText(elapse int64) string {
+	h := elapse / 3600
+	elapse %= 3600
+	m := elapse / 60
+	s := elapse % 60
+	return fmt.Sprintf("%02d:%02d:%02d", h, m, s)
 }
