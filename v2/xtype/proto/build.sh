c1be6ee1b1f85a38042bdf48a17fba8095dd5a3d
yxz6 <yxzbox@gmail.com>
2022-12-31 14:23:29 -0500

    fast commit

---
+++ v2/xtype/proto/build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+SCRIPT_DIR=$(dirname "$0")
+
+protoc base.proto --go_out="$SCRIPT_DIR" && mv "$SCRIPT_DIR"/code.olapie.com/sugar/v2/xtype/base.pb.go "$SCRIPT_DIR"/../
+rm -r "$SCRIPT_DIR"/code.olapie.com
