1324b8084dd1f58cd1b57d5498824784fee47ce6
yxz6 <yxzbox@gmail.com>
2023-01-14 17:24:41 -0500

    fast commit

--- v2/mob/device.go
+++ v2/mob/device.go
@@ -1,16 +1,30 @@
 package mob
 
-type DeviceInfo struct {
-	Name       string `json:"name"`
-	Model      string `json:"model"`
-	ModelType  string `json:"model_type"`
-	Language   string `json:"language"`
-	SysVersion string `json:"sys_version"`
-	Carrier    string `json:"carrier"`
+import (
+	"code.olapie.com/sugar/v2/xjson"
+	"encoding/json"
+)
 
-	AppVersion string `json:"app_version"`
+type DeviceInfo struct {
+	Name       string `json:"name,omitempty"`
+	Model      string `json:"model,omitempty"`
+	ModelType  string `json:"model_type,omitempty"`
+	Language   string `json:"language,omitempty"`
+	SysVersion string `json:"sys_version,omitempty"`
+	Carrier    string `json:"carrier,omitempty"`
+
+	AppVersion string `json:"app_version,omitempty"`
 }
 
 func NewDeviceInfo() *DeviceInfo {
 	return new(DeviceInfo)
+}
+
+func (d *DeviceInfo) Attributes() map[string]string {
+	m := make(map[string]string)
+	err := json.Unmarshal(xjson.ToBytes(d), &m)
+	if err != nil {
+		panic(err)
+	}
+	return m
 }
