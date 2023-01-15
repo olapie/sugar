485068b487b05cff87b4db14cda16e6c4464ca80
yxz6 <yxzbox@gmail.com>
2023-01-15 14:18:15 -0500

    fast commit

--- v2/tools/psqlcode/main.go
+++ v2/tools/psqlcode/main.go
@@ -5,6 +5,32 @@ import (
 	"os"
 )
 
+/*
+- name: user
+  table: test.users
+  primaryKey:
+    - id
+  columns:
+    id: int64
+    name: string
+    gender: xtype.Gender
+    dob: string
+    place: xtype.Place
+    accounts: "[]string"
+  jsonColumns:
+    - place
+- name: class_user
+  table: test.class_users
+  primaryKey:
+    - class_id
+    - user_id
+  columns:
+    class_id: int64
+    user_id: int64
+    created_time: time.Time
+    score: float64
+*/
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Printf("Usage: %s {modelFilename}", os.Args[0])
