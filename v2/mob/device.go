102bc064ff5a38cf6e3c55da186d888e7847bbad
yxz6 <yxzbox@gmail.com>
2023-01-15 12:49:44 -0500

    fast commit

--- v2/mob/device.go
+++ v2/mob/device.go
@@ -29,7 +29,7 @@ func (d *DeviceInfo) Attributes() map[string]string {
 }
 
 type AppInfo struct {
-	ID      string `json:"id,omitempty"`
+	AppID   string `json:"id,omitempty"`
 	Name    string `json:"name,omitempty"`
 	Version string `json:"version,omitempty"`
 }
