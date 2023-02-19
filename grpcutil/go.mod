44d75d138d00491f2718aa8f46b2bcca3ce78a6f
yxz6 <yxzbox@gmail.com>
2023-02-19 17:37:49 -0500

    fast commit

---
+++ grpcutil/go.mod
@@ -0,0 +1,24 @@
+module code.olapie.com/sugar/grpcutil
+
+go 1.20
+
+require (
+	code.olapie.com/log v1.1.6
+	code.olapie.com/sugar/v2 v2.3.12
+	google.golang.org/grpc v1.53.0
+)
+
+require (
+	github.com/golang/geo v0.0.0-20210211234256-740aa86cb551 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/natefinch/lumberjack/v3 v3.0.0-alpha // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
+	go.uber.org/zap v1.24.0 // indirect
+	golang.org/x/net v0.5.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/text v0.6.0 // indirect
+	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
---
+++ grpcutil/util.go
@@ -0,0 +1,84 @@
+package grpcutil
+
+import (
+	"context"
+	"net/http"
+	"reflect"
+	"time"
+
+	"code.olapie.com/log"
+	"code.olapie.com/sugar/v2/ctxutil"
+	"code.olapie.com/sugar/v2/httpkit"
+	"code.olapie.com/sugar/v2/xerror"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
+
+// Refer to https://github.com/grpc/grpc/blob/master/doc/http-grpc-status-mapping.md
+
+var statusErrorType = reflect.TypeOf(status.Error(codes.Unknown, ""))
+var statusToCodeMap = map[int]codes.Code{
+	http.StatusUnauthorized: codes.Unauthenticated,
+	http.StatusForbidden:    codes.PermissionDenied,
+	http.StatusBadRequest:   codes.InvalidArgument,
+	http.StatusNotFound:     codes.NotFound,
+	http.StatusConflict:     codes.AlreadyExists,
+
+	http.StatusNotImplemented: codes.Unimplemented,
+
+	http.StatusInternalServerError: codes.Internal,
+	http.StatusBadGateway:          codes.Unavailable,
+	http.StatusServiceUnavailable:  codes.Unavailable,
+}
+
+func ServerStart(ctx context.Context, info *grpc.UnaryServerInfo, verifier httpkit.Verifier) (context.Context, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return ctx, status.Error(codes.InvalidArgument, "failed reading request metadata")
+	}
+
+	header := http.Header(md)
+
+	if !verifier.Verify(ctx, header) {
+		return ctx, status.Error(codes.InvalidArgument, "failed verifying")
+	}
+
+	traceID := httpkit.GetTraceID(header)
+	ctx = ctxutil.Request(ctx).WithAppID(httpkit.GetAppID(header)).
+		WithClientID(httpkit.GetClientID(header)).
+		WithTraceID(traceID).
+		Build()
+	logger := log.FromContext(ctx).With(log.String("trace_id", traceID))
+	fields := make([]log.Field, 0, len(md)+1)
+	fields = append(fields, log.String("full_method", info.FullMethod))
+	for k, v := range md {
+		if len(v) == 0 {
+			continue
+		}
+		fields = append(fields, log.String(k, v[0]))
+	}
+	logger.Info("start", fields...)
+	ctx = log.BuildContext(ctx, logger)
+	return ctx, nil
+}
+
+func ServerFinish(resp any, err error, logger *log.Logger, startAt time.Time) (any, error) {
+	if err == nil {
+		logger.Info("finished", log.Duration("cost", time.Now().Sub(startAt)))
+		return resp, nil
+	}
+
+	logger.Error("failed", log.Err(err))
+
+	if reflect.TypeOf(err) == statusErrorType {
+		return nil, err
+	}
+
+	code, ok := statusToCodeMap[xerror.GetCode(err)]
+	if !ok {
+		code = codes.Unknown
+	}
+	return nil, status.Error(code, err.Error())
+}
