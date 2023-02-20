a2b20338f38ce6969a97f2a99f8797beaa5661cf
yxz6 <yxzbox@gmail.com>
2023-02-20 16:02:24 -0500

    fast commit

--- httpkit/kit.go
+++ httpkit/kit.go
@@ -3,6 +3,7 @@ package httpkit
 import (
 	"code.olapie.com/sugar/v2/jsonutil"
 	"code.olapie.com/sugar/v2/xerror"
+	"io"
 	"log"
 	"net/http"
 	"strconv"
@@ -37,5 +38,19 @@ func WriteJSON(w http.ResponseWriter, v any) {
 	_, err := w.Write(jsonutil.ToBytes(v))
 	if err != nil {
 		log.Println(err)
+	}
+}
+
+func StreamFile(w http.ResponseWriter, name string, f io.ReadCloser) {
+	defer f.Close()
+	SetContentType(w.Header(), OctetStream)
+	if name != "" {
+		w.Header().Set(KeyContentDisposition, ToAttachment(name))
+	}
+	_, err := io.Copy(w, f)
+	if err != nil {
+		if err != io.EOF {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
 	}
 }
